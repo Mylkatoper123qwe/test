957b11584982d78ccc7eaea0c7b6300c8f2cd95a
nshs33-102 <ualixzzy@gmail.com>
2022-12-03 12:50:52 +0200

    Готово

--- main.py
+++ main.py
@@ -13,8 +13,13 @@ class Bus:
 
 
     def print_passengers(self):
-        for passenger in self.passengers:
-            print(passenger.name)
+        if self.passengers != []:
+            print(f"У автобусі {self.model} є такі пасажири ")
+            for passenger in self.passengers:
+                print(passenger.name)
+
+        else:
+            print(f"У автобусі{self.model} немає пасажирів.")
 
 
 tomato = Bus("tomato","black")
@@ -23,5 +28,4 @@ tomato.add_passengers(human("Oleg"))
 tomato.add_passengers(human("Marek"))
 tomato.add_passengers(human("Dmytro"))
 tomato.add_passengers(human("Taras"))
-
 tomato.print_passengers()
